c8c6aa3eff9dd03a3da11eb96046a7d47f8c0c0a
Thakkar Khush <104725303+khush9122002@users.noreply.github.com>
2024-10-16 08:44:17 -0800

    Add files via upload

---
+++ learn python.py
@@ -0,0 +1,102 @@
+# input and output 
+# var=int(input("enter message :: "))
+# print(var)
+# print(type(var)) # defult input store string value
+
+
+#?
+#int b=>1,2,3
+#float b=>1.2,3.1
+#dictionary b=>{'a':'jigar'}
+#list=[1,2,3]
+#tuple=(1,2,3)
+#set={1,2,3}
+# var=eval("{}")
+print("herllo")
+print("world")
+# print("hello","word","khush","=============1")
+# print("hello","word","khush","=============2",sep="/")
+
+# print("hello",end=" di  ")
+# print("world")
+#list into------------------------  whatshap group, solve error, keypoint share make repo in git
+l=['a','x','b','v',1,2,3,4,5]
+# print(l[-1:2]) # why gives []
+print(l[:6:2])
+# l[3]=7 # --replace 
+# l.insert(3,0) # insert
+# print(l[1:])
+# l.sort(reverse=True)
+# l.reverse()
+# print(l)
+exit()
+# truple
+t=(1,2,4,2)
+# print(t.index(2))
+# print(t.count(2))
+
+# test
+# m1=input()
+# m2=input()
+# m3=input()
+# l=[m1,m2,m3]
+# print(l)
+# print(type(l))
+# check polindrom or not
+# l=list(input())
+# cl=l.copy()
+# cl.reverse()
+# if l==cl:
+#     print("palindrom",cl)
+# else:
+#     print("not palindrom",cl)
+
+# number of count a 
+t=['d',"a","b",'c','c','a','a']
+# print(t.count('a'))
+# t.sort()
+# print(t)
+
+dict={
+    "name":"khush",
+    "animals":{
+        "cat":1,
+        "horse":2
+    }
+}
+# print(dict["animals"]["cat"])
+
+# dict={
+#     m1:m1,
+#     "sub2":m2,
+#     "sub3":m3
+# }
+# print(dict)
+
+# stor 9 and 9.0 in set
+s={9,9.0}
+s={9,"9.0"}
+s={
+    ("float",9.0),
+    ("int",9)
+}
+# print(s)
+
+a=[21,33,222,232]
+b=6
+# if a>b:
+#     print(a)
+# elif b>a:
+#     # print(b)
+# else:
+#     print("no")
+
+# while a<10:
+#     print(a)
+#     a+=1
+# else:
+#     print("no match ...")
+
+# for i in b:
+#     print(i)
+
